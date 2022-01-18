74e66eb43ab1de98a8e095d530b57ffd48621970
AntoniaB21 <antonia.balluais@mail-ecv.fr>
2022-01-18 22:33:53 +0100

    test entities

---
+++ src/Entity/Test.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TestRepository;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=TestRepository::class)
+ */
+class Test
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $message;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
