676d89b98260cd463fef63b5f6d8865f49a34ece
= <antonia.balluais@mail-ecv.fr>
2022-02-23 23:41:19 +0100

    remove /api in path

--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiSubresource;
 use App\Repository\TagRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -32,6 +33,7 @@ class Tag
     private $slug;
 
     /**
+     * @ApiSubresource()
      * @ORM\OneToMany(targetEntity=TagChildren::class, mappedBy="tag")
      */
     private $tagChildrens;
