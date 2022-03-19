d27aab9ee9d4f6afca8ffa7087e34f7d79d43682
= <antonia.balluais@mail-ecv.fr>
2022-03-19 20:23:30 +0100

    security post tag

--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -5,9 +5,25 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\TagRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
- * @ApiResource()
+ * @ApiResource(attributes={
+ *      "normalization_context"={"groups"={"tag:read", "enable_max_depth"=true}},
+ *      "denormalization_context"={"groups"={"tag:write"}},
+ *      "pagination_items_per_page"=20
+ * },
+ *  collectionOperations={
+ *      "get",
+ *      "post"={
+ *          "security"="is_granted('ROLE_ADMIN')",
+ *          "security_message"="Only admin can add tags",
+ *      }
+ * },
+ *  itemOperations={
+ *      "get",
+ *      "put"
+ * })
  * @ORM\Entity(repositoryClass=TagRepository::class)
  */
 class Tag
@@ -20,11 +36,13 @@ class Tag
     private $id;
 
     /**
+     * @Groups({"tag:read", "tag:write"})
      * @ORM\Column(type="string", length=50)
      */
     private $name;
 
     /**
+     * @Groups({"tag:read", "tag:write"})
      * @ORM\Column(type="string", length=100)
      */
     private $slug;
