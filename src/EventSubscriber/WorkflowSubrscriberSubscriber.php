031c17c59ff9821fda7d7b925903368a8b713163
= <antonia.balluais@mail-ecv.fr>
2022-04-18 18:43:03 +0200

    send mail and add notification on response offrequest

--- src/EventSubscriber/WorkflowSubrscriberSubscriber.php
+++ src/EventSubscriber/WorkflowSubrscriberSubscriber.php
@@ -2,11 +2,13 @@
 
 namespace App\EventSubscriber;
 
+use App\Entity\Notifications;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Workflow\Event\GuardEvent;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Workflow\Event\Event;
+use Doctrine\ORM\EntityManagerInterface;
 
 
 
@@ -17,29 +19,38 @@ class WorkflowSubrscriberSubscriber implements EventSubscriberInterface
     //     $event->setBlocked(true, "I am not allowed to do this :(");
     // }
     private $mailer;
+    private $entityManager;
 
-    public function __construct(MailerInterface $mailer)
+    public function __construct(MailerInterface $mailer, EntityManagerInterface $entityManager)
     {
         $this->mailer = $mailer;
+        $this->entityManager = $entityManager;
+
     }
 
-    public function AfterStatusUpdated(Event $event){
+    public function AcceptedStatusUpdated(Event $event){
         // envoyer un email quand le status est à jour
-        dd($event->getSubject()->getUser()->getEmail());
         $destinataire = $event->getSubject()->getUser()->getEmail();
-        $email = (new Email())
-        ->to('admin282828@yopmail.fr')
-        ->subject('Demande de congés')
-        ->text('Salut tu as ta réponse de demande de congés');
-        
-        $this->mailer->send($email);
-    }
 
+        // $email = (new Email())
+        // ->from('antonia.balluais@vmed.fr')
+        // ->to('admin282828@yopmail.fr')
+        // ->subject('Demande de congés '. $event->getSubject()->getStatus())
+        // ->text('Votre demande de congés a été '. $event->getSubject()->getStatus());
+        // $this->mailer->send($email);
+
+        // create notification
+        $notification = new Notifications();
+        $notification->setMessage('Votre demande de congés a été '. $event->getSubject()->getStatus());
+        $notification->setUser($event->getSubject()->getUser());
+        $this->entityManager->persist($notification);
+        $this->entityManager->flush();
+    }
+    
     public static function getSubscribedEvents()
     {
         return [
-            // 'workflow.off_request_validation.guard' => 'onWorkflowOffRequestValidationGuard',
-            'workflow.off_request_validation.entered' => 'AfterStatusUpdated',
+            'workflow.off_request_validation.entered' => 'AcceptedStatusUpdated',
         ];
     }
 }
