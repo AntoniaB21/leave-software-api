6cefe0ae52f2086973c94ae5ebcfaf1c868019af
= <antonia.balluais@mail-ecv.fr>
2022-04-23 22:13:08 +0200

    fix user authentication

--- src/Listeners/OnAuthSuccessListener.php
+++ src/Listeners/OnAuthSuccessListener.php
@@ -2,9 +2,45 @@
 
 namespace App\Listeners;
 
-class OnAuthSuccessListener {
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Response\JWTAuthenticationSuccessResponse;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
-    public function onAuthenticationSuccess(){
-        dd("Authentication sucess");
+class OnAuthSuccessListener {
+    private $jwtTokenTTL;
+
+    private $cookieSecure = false;
+
+    public function __construct($ttl)
+    {
+        $this->jwtTokenTTL = $ttl;
+    }
+
+    /**
+     * This function is responsible for the authentication part
+     *
+     * @param AuthenticationSuccessEvent $event
+     * @return JWTAuthenticationSuccessResponse
+     */
+    public function onAuthenticationSuccess(AuthenticationSuccessEvent $event)
+    {
+        // /** @var JWTAuthenticationSuccessResponse $response */
+
+        // $response = $event->getResponse();
+        $data = $event->getData();
+        dd($data);
+        // $tokenJWT = $data['token'];
+        // // unset($data['token']);
+        // // unset($data['refresh_token']);
+        // $event->setData($data);
+
+        // $response->headers->setCookie(new Cookie('New', $tokenJWT, (
+        //     new \DateTime())
+        //     ->add(new \DateInterval('PT' . $this->jwtTokenTTL . 'S')), '/', null, $this->cookieSecure));
+        
+            
+        return $event;
     }
 }
